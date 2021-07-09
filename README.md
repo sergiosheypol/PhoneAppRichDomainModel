d729dc3cbc847238921954b0fe77e33d2048e857
Sergio Sheypol <sheypolmusic@gmail.com>
2021-07-09 16:34:29 +0200

    Tweaks

--- README.md
+++ README.md
@@ -3,4 +3,47 @@
 [![Vertx](https://img.shields.io/badge/vert.x-4.0.3-purple.svg)](link="https://vertx.io")
 
 This project aims to serve as an example on how to refactor a Repository-Service type of application
-to a Rich Domain Model type.
+to a Rich Domain Model type to step away from old monolithic Java designs.
+
+It uses MongoDB. Since this is an example, we'll use a Dockerized Mongo. See [mongo folder](/mongo).
+
+## Tech Stack
+* Java 11
+* Vert.x 4.0.3
+* MongoDB
+* [Bazel](https://bazel.build)
+* Gradle
+
+## Requisites
+
+* Java 11
+* Docker
+
+Optional:
+* Bazel
+
+## How to run
+
+I'm using [Bazel](https://bazel.build) for running it but Gradle is also available for IntelliJ support.
+
+The app starts on the port 80
+
+### Prerequisites
+1. Initialize mongo by running the `mongo/init.sh` script. Don't forget to use `mongo/kill.sh` to delete the container
+
+### Running with Gradle and IntelliJ
+1. Open `App.java` and hit RUN in IntelliJ (executing the Main method)
+
+### Running with Bazel
+1. `cd catalog`
+1. `bazel run :service`
+
+
+
+## Endpoints
+* GET: /catalog
+
+### Example cURL
+```
+curl --location --request GET 'localhost:80/catalog'
+```
--- catalog/src/main/java/com/shpl/catalog/App.java
+++ catalog/src/main/java/com/shpl/catalog/App.java
@@ -1,9 +1,41 @@
 package com.shpl.catalog;
 
-public class App extends DefaultMain {
+import io.reactivex.Single;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.Vertx;
 
+public class App {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+  @SuppressWarnings("CheckReturnValue")
   public static void main(String[] args) {
-    run(new CatalogVerticle());
+
+    LOGGER.info("\n----------------------- \n Booting app\n-----------------------");
+
+    // Read properties
+    config(Vertx.vertx()).flatMap(json -> Vertx.currentContext().owner()
+      .rxDeployVerticle(new CatalogVerticle(), new DeploymentOptions().setConfig(json)))
+      .subscribe(id -> LOGGER.info(String.format("Verticle with id {%s} deployed successfully", id)),
+        err -> LOGGER.error("Error deploying verticle", err));
+
   }
 
+  private static Single<JsonObject> config(final Vertx vertx) {
+    final ConfigStoreOptions store = new ConfigStoreOptions()
+      .setType("file")
+      .setFormat("yaml")
+      .setConfig(new JsonObject()
+        .put("path", "config.yaml")
+      );
+
+    return ConfigRetriever.create(vertx, new ConfigRetrieverOptions().addStore(store)).rxGetConfig();
+
+  }
 }
