4157c472e577c9143236e9a21fa771f1178741f9
Sergio Sheypol <sheypolmusic@gmail.com>
2021-07-19 17:44:12 +0200

    Minor fix

--- catalog/src/main/java/com/shpl/catalog/Phone.java
+++ catalog/src/main/java/com/shpl/catalog/Phone.java
@@ -4,11 +4,16 @@ import io.vertx.core.json.JsonObject;
 
 public final class Phone {
 
+  // PHONE MODEL
   public static final String ID = "id";
   public static final String NAME = "name";
   public static final String IMG = "img";
   public static final String DESCRIPTION = "description";
   public static final String PRICE = "price";
+
+  // PRICE MODEL
+  public static final String CURRENCY = "currency";
+  public static final String VALUE = "value";
 
   private static final String MONGO_ID = "_id";
 
@@ -19,7 +24,7 @@ public final class Phone {
   }
 
   public static Phone fromMongo (final JsonObject json) {
-    final var model = json.copy();
+    final JsonObject model = json.copy();
     model.remove(MONGO_ID);
     model.put(ID, json.getString(MONGO_ID));
     return new Phone(model);
